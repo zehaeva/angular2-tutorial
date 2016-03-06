d5260a77ce292dd484baa570b608c6b51c50334b
Howard Canaway <howard.canaway@gmail.com>
2016-03-06 11:51:56 -0500

    removed the data from app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -72,23 +72,9 @@ import {HeroDetailComponent} from './hero-detail.component';
 
 export class AppComponent {
     public title = 'Tour of Heroes';
-    public heroes = HEROES;
+    public heroes = Hero[];
 
     public selectedHero: Hero;
     onSelect(hero: Hero) { this.selectedHero = hero; }
 }
-
-var HEROES: Hero[] = [
-    { "id": 11, "name": "Mr. Nice" },
-    { "id": 12, "name": "Narco" },
-    { "id": 13, "name": "Bombasto" },
-    { "id": 14, "name": "Celeritas" },
-    { "id": 15, "name": "Magneta" },
-    { "id": 16, "name": "Rubber Man" },
-    { "id": 17, "name": "Dynama" },
-    { "id": 18, "name": "Dr I.Q." },
-    { "id": 19, "name": "Magma" },
-    { "id": 20, "name": "Gambler" },
-    { "id": 21, "name": "Tornado" }
-];
 
